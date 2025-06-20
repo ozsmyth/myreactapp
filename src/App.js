36179f5748ba88e25db9f15239ffe2100be71e4a
ozsmyth <52673240+ozsmyth@users.noreply.github.com>
2025-06-20 05:14:37 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Weather from './components/Weather';
+import './css/index.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
